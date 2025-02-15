4fdeabbf63e949bbec033f7df425c8d3ca65b73b
ant0d0v <gosu1203@gmail.com>
2025-02-15 23:42:53 +0200

    change Page Suffix

--- app/PageSuffix.ts
+++ app/PageSuffix.ts
@@ -4,9 +4,9 @@ export enum PageSuffix {
     COMPARE_PAGE = "/compare",
     HOME_PAGE = "/",
     MAC_PAGE = "/mac",
-    MACBOOK_AIR_PAGE = "/macbook-air"
+    MACBOOK_AIR_PAGE = "/mac/macbook-air"
 }
 
 export const MacBookAir= {
-    M1_256GB_13_8GB_SERYY_KOSMOS: `${PageSuffix.MAC_PAGE}${PageSuffix.MACBOOK_AIR_PAGE}/macbook-air-13-8-gb-256-gb-apple-m1-seryy-kosmos`
+    M1_256GB_13_8GB_SERYY_KOSMOS: `${PageSuffix.MACBOOK_AIR_PAGE}/macbook-air-13-8-gb-256-gb-apple-m1-seryy-kosmos`
 };
